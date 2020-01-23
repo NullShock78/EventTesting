0c0a86d4acd7e2ae2868a9f65962b174cc74dcfc
kstamant <kylestamant1@gmail.com>
2020-01-22 20:48:58 -0500

    initial

---
+++ SerializeTest/SerializeTest/EventWarlock/Warlock.cs
@@ -0,0 +1,77 @@
+using SerializeTest.EventWarlock;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTCV.CorruptCore.EventWarlock
+{
+    /// <summary>
+    /// I'm a wizard hagrid
+    /// </summary>
+    static class Warlock
+    {
+        public static Grimoire BookOfSpells = new Grimoire();
+
+        public static void SaveGrimoire(string path)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            Stream s = new FileStream(path, FileMode.Create, FileAccess.Write);
+            formatter.Serialize(s, BookOfSpells);
+            s.Close();
+        }
+
+        public static void LoadGrimoire(string path)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            Stream s = new FileStream(path, FileMode.Open, FileAccess.Read);
+            BookOfSpells = (Grimoire)formatter.Deserialize(s);
+            s.Close();
+        }
+
+
+        public static void AddLoadSpell(Spell spell)
+        {
+            BookOfSpells.LoadSpells.Add(spell);
+        }
+
+        public static void AddPreExecuteSpell(Spell spell)
+        {
+            BookOfSpells.PreExecuteSpells.Add(spell);
+        }
+
+        public static void AddPostExecuteSpell(Spell spell)
+        {
+            BookOfSpells.PostExecuteSpells.Add(spell);
+        }
+
+
+        public static void Load()
+        {
+            for (int j = 0; j < BookOfSpells.LoadSpells.Count; j++)
+            {
+                BookOfSpells.LoadSpells[j].Execute();
+            }
+        }
+
+        public static void PreExecute()
+        {
+            for (int j = 0; j < BookOfSpells.PreExecuteSpells.Count; j++)
+            {
+                BookOfSpells.PreExecuteSpells[j].Execute();
+            }
+        }
+
+        public static void PostExecute()
+        {
+            for (int j = 0; j < BookOfSpells.PostExecuteSpells.Count; j++)
+            {
+                BookOfSpells.PostExecuteSpells[j].Execute();
+            }
+        }
+    }
+}
---
+++ SerializeTest/SerializeTest/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RTCV.CorruptCore.EventWarlock;
+using RTCV.CorruptCore.EventWarlock.WarlockActions;
+using SerializeTest.EventWarlock.WarlockConditions;
+
+namespace SerializeTest
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var k = Console.ReadKey().KeyChar.ToString().ToLower()[0];
+            Console.WriteLine();
+            if (k == 's')
+            {
+                Spell a = new Spell();
+                var eqA = new FirstEqualsSecond("a", "a");
+                a.SetConditional(eqA);
+                a.AddAction(new WarlockActionEcho("Test"));
+                a.AddAction(new WarlockActionEcho("Test 1"));
+                a.AddAction(new WarlockActionEcho("Test 2"));
+                Warlock.AddLoadSpell(a);
+
+                Spell b = new Spell();
+                var eqB = new FirstEqualsSecond("a", "b");
+                b.SetConditional(eqB);
+                b.AddAction(new WarlockActionEcho("Test 3"));
+                Warlock.AddPreExecuteSpell(b);
+
+                Spell c = new Spell();
+                var eqC = new EWConditionGroup();
+                eqC.AddConditional(new FirstEqualsSecond("a", "a"));
+                eqC.AddOperator(QuestionOp.AND);
+                eqC.AddConditional(new FirstEqualsSecond("c", "c"));
+                c.SetConditional(eqC);
+                c.AddAction(new WarlockActionEcho("Test 4"));
+                c.AddAction(new WarlockActionEcho("Test 5"));
+                Warlock.AddPostExecuteSpell(c);
+
+                Warlock.SaveGrimoire("./grimoire.dat");
+            }
+            else if (k == 'l')
+            {
+                Warlock.LoadGrimoire("./grimoire.dat");
+            }
+
+
+
+            Console.WriteLine("Warlock Load");
+            Warlock.Load();
+
+            Console.WriteLine("Warlock PreExecute");
+            Warlock.PreExecute();
+
+            Console.WriteLine("Warlock PostExecute");
+            Warlock.PostExecute();
+
+            Console.ReadKey();
+        }
+    }
+}
